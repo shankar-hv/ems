68f614e343896fe755a6314815f0b1a4a9c37f02
HV Shankar <vedashankar1617@gmail.com>
2026-05-24 22:22:56 +0530

    fix render deployment

--- employee_management_system/settings.py
+++ employee_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q$8ytx44v(t1%_j11h*s=^3-ito==kadqtviab8fsbd()&bxba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
